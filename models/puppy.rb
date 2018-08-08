e8f3660dc8ad4a92928f2387d8064ab545f70d17
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 13:51:33 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,10 @@
-class Puppy
+class Puppy 
 
   attr_accessor :name, :breed, :age 
 
   def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
-
-end
+    self.name = name 
+    self.breed = breed 
+    self.age = age 
+  end 
+end 
